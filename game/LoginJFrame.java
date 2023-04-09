fbafa42cc2217df16522c6bfc419e14599baecbf
Jack Wei <jack_weq@icloud.com>
2023-04-09 16:14:05 -0400

    update

--- game/LoginJFrame.java
+++ game/LoginJFrame.java
@@ -6,16 +6,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class LoginJFrame {
+public class LoginJFrame extends JFrame{
 
     static ArrayList<User> allUsers = new ArrayList<>();
 
     static{
         allUsers.add(new User("jack", "123"));
     }
-
-    JFrame frame = new JFrame();
-
 
     JButton login = new JButton();
     JButton register = new JButton();
@@ -29,18 +26,16 @@ public class LoginJFrame {
         initJFrame();
         initView();
 
-        frame.setVisible(true);
+        this.setVisible(true);
     }
 
-
-
     private void initJFrame() {
-        frame.setSize(633, 423);
-        frame.setTitle("Poker Login");
-        frame.setDefaultCloseOperation(3);
-        frame.setLocationRelativeTo(null);
-        frame.setAlwaysOnTop(true);
-        frame.setLayout(null);
+        this.setSize(633, 423);
+        this.setTitle("Poker Login");
+        this.setDefaultCloseOperation(3);
+        this.setLocationRelativeTo(null);
+        this.setAlwaysOnTop(true);
+        this.setLayout(null);
     }
 
     private void initView() {
@@ -50,10 +45,10 @@ public class LoginJFrame {
         usernameText.setForeground(Color.WHITE); // TODO: whats the diff?
         usernameText.setFont(usernameFont);
         usernameText.setBounds(140, 55, 55, 22);
-        frame.getContentPane().add(username);
+        this.getContentPane().add(username);
 
         username.setBounds(223, 46, 200, 30);
-        frame.getContentPane().add(username);
+        this.getContentPane().add(username);
 
         // password
         Font passwordFont = new Font(null, 1, 16);
@@ -61,11 +56,12 @@ public class LoginJFrame {
         passwordText.setForeground(Color.WHITE); // TODO: whats the diff?
         passwordText.setFont(passwordFont);
         passwordText.setBounds(140, 55, 55, 22);
-        frame.getContentPane().add(password);
+        this.getContentPane().add(password);
 
         password.setBounds(263, 87, 160, 30);
-        frame.getContentPane().add(password);
+        this.getContentPane().add(password);
 
+        // code
 
     }
 }
